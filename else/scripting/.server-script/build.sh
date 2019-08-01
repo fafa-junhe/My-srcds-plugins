fb67db3ebf94f8d619f3081cb7ce5f2ebdab9218
fafa_junhe <842355358@qq.com>
2019-08-01 08:56:33 +0800

    upload

--- else/scripting/.server-script/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# This script runs on the server
-# A server script file can have any name, set name in .server-script/server-setup.cson
-# replace the contents of this file with your server script or replace the file entirely.
-
-echo "Test of server-script was successful"
